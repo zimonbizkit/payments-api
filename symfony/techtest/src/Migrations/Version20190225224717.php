fee620101d21bde99418e8f653ecc15e2173fea2
zimonbizkit <simon.gallegos.edu@gmail.com>
2019-02-26 19:02:34 +0100

    adding migrations

---
+++ symfony/techtest/src/Migrations/Version20190225224717.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190225224717 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("create table user
+(
+	id varchar(40) not null
+		primary key,
+	name varchar(50) null,
+	balance float default '0' null
+)
+comment 'The user table with its fields' engine=InnoDB
+;
+
+");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("drop table user");
+    }
+}
